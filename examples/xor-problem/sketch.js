e5690d407e14a9a5c61c88dafdc6f5fad7b5ddf7
viktorstrate <viktorstrate@gmail.com>
2018-09-20 15:44:19 +0200

    Add examples folder with xor problem

---
+++ examples/xor-problem/sketch.js
@@ -0,0 +1,57 @@
+let nn
+let lr_slider
+
+let training_data = [
+  {
+    inputs: [0, 0],
+    targets: [1],
+  },
+  {
+    inputs: [1, 1],
+    targets: [1],
+  },
+  {
+    inputs: [0, 1],
+    targets: [0],
+  },
+  {
+    inputs: [1, 0],
+    targets: [0],
+  },
+]
+
+function setup() {
+  createCanvas(400, 400)
+  background(200)
+
+  nn = new NeuralNetwork(2, 4, 1)
+
+  createSpan('Learning Rate:')
+  lr_slider = createSlider(0, 0.5, 0.1, 0.01)
+}
+
+function draw() {
+  nn.learning_rate = lr_slider.value()
+
+  for (let i = 0; i < 50; i++) {
+    let data = random(training_data)
+    nn.train(data.inputs, data.targets)
+  }
+
+  let resolution = 10
+  let cols = width / resolution
+  let rows = height / resolution
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x1 = i / cols
+      let x2 = j / rows
+
+      let y = nn.feedforward([x1, x2])
+      fill(y * 255)
+      strokeWeight(0)
+
+      rect(i * resolution, j * resolution, resolution, resolution)
+    }
+  }
+}
